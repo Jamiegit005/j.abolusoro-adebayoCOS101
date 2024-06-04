dc3d112f297d3ed79b68e4a858254742490d0161
Jamiegit005 <146426613+Jamiegit005@users.noreply.github.com>
2024-06-04 12:35:31 +0100

    Delete week-6/project_1 directory

--- week-6/project_1/target/debug/deps/project_1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\H P\Documents\j.abolusoro-adebayoCOS101\week-6\project_1\target\debug\deps\project_1.exe: src\main.rs
-
-C:\Users\H P\Documents\j.abolusoro-adebayoCOS101\week-6\project_1\target\debug\deps\project_1.d: src\main.rs
-
-src\main.rs:
--- week-6/project_1/target/debug/project_1.d
+++
@@ -1,1 +0,0 @@
-C:\Users\H\ P\Documents\j.abolusoro-adebayoCOS101\week-6\project_1\target\debug\project_1.exe: C:\Users\H\ P\Documents\j.abolusoro-adebayoCOS101\week-6\project_1\src\main.rs
